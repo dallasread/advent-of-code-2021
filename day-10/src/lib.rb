bce78127f22dafe95d160148358b924bb2962ce8
Dallas Read <dallas@excitecreative.ca>
2021-12-10 08:40:19 -0400

    Day 10: Star 2

--- day-10/src/lib.rb
+++ day-10/src/lib.rb
@@ -2,14 +2,61 @@
 
 require 'test/unit/assertions'
 
+class InvalidChunkError < StandardError
+  attr_reader :expected, :invalid
+
+  POINTS = {
+    ')' => 3,
+    ']' => 57,
+    '}' => 1197,
+    '>' => 25_137
+  }.freeze
+
+  def initialize(expected:, invalid:)
+    super('Invalid chunk')
+    @expected = expected
+    @invalid = invalid
+  end
+
+  def points
+    POINTS[invalid]
+  end
+end
+
+class Score
+  attr_reader :chars
+
+  POINTS = {
+    ')' => 1,
+    ']' => 2,
+    '}' => 3,
+    '>' => 4
+  }.freeze
+
+  def initialize(chars:)
+    @chars = chars.split('')
+  end
+
+  def points
+    points = 0
+
+    chars.each do |char|
+      points *= 5
+      points += POINTS[char]
+    end
+
+    points
+  end
+end
+
 class Line
   attr_reader :chars
 
   OPENERS = ['(', '[', '{', '<'].freeze
   CLOSERS = [')', ']', '}', '>'].freeze
 
-  def initialize(value:)
-    @chars = value.split('')
+  def initialize(chars:)
+    @chars = chars.split('')
   end
 
   def parse
@@ -29,26 +76,34 @@ class Line
       end
     end
   end
-end
 
-class InvalidChunkError < StandardError
-  attr_reader :expected, :invalid
-
-  POINTS = {
-    ')' => 3,
-    ']' => 57,
-    '}' => 1197,
-    '>' => 25_137
-  }.freeze
-
-  def initialize(expected:, invalid:)
-    super('Invalid chunk')
-    @expected = expected
-    @invalid = invalid
+  def finish_score
+    Score.new(chars: finish_chars)
+  rescue InvalidChunkError
+    Score.new(chars: '')
   end
 
-  def points
-    POINTS[invalid]
+  def finish_chars
+    buffer = []
+
+    chars.each do |char|
+      if OPENERS.include?(char)
+        buffer << char
+      elsif CLOSERS.include?(char)
+        opener = buffer.pop
+        opener_index = OPENERS.find_index(opener)
+        expected = CLOSERS[opener_index]
+
+        next if char == expected
+
+        raise InvalidChunkError.new(invalid: char, expected: expected)
+      end
+    end
+
+    buffer.map do |char|
+      opener_index = OPENERS.find_index(char)
+      CLOSERS[opener_index]
+    end.join.reverse
   end
 end
 
@@ -76,9 +131,15 @@ class Calculator
   end
 
   def self.from_raw(input)
-    lines = input.split("\n").map { |line| Line.new(value: line) }
+    lines = input.split("\n").map { |line| Line.new(chars: line) }
 
     Calculator.new(lines: lines)
+  end
+
+  def self.find_middle_score(scores:)
+    points = scores.map(&:points).reject(&:zero?).sort
+    index = (points.size / 2).floor
+    points[index]
   end
 end
 
@@ -90,5 +151,28 @@ describe Calculator do
     calc = Calculator.from_raw(input)
 
     assert_equal 26_397, Calculator.sum_points(chunks: calc.collect_invalid_chunks)
+  end
+
+  it 'calculates the score' do
+    assert_equal 288_957, Score.new(chars: '}}]])})]').points
+    assert_equal 5566, Score.new(chars: ')}>]})').points
+    assert_equal 1_480_781, Score.new(chars: '}}>}>))))').points
+    assert_equal 995_444, Score.new(chars: ']]}}]}]}>').points
+    assert_equal 294, Score.new(chars: '])}>').points
+  end
+
+  it 'finishes a line' do
+    assert_equal '}}]])})]', Line.new(chars: '[({(<(())[]>[[{[]{<()<>>').finish_chars
+    assert_equal ')}>]})', Line.new(chars: '[(()[<>])]({[<{<<[]>>(').finish_chars
+    assert_equal '}}>}>))))', Line.new(chars: '(((({<>}<{<{<>}{[]{[]{}').finish_chars
+    assert_equal ']]}}]}]}>', Line.new(chars: '{<[[]]>}<{[{[{[]{()[[[]').finish_chars
+    assert_equal '])}>', Line.new(chars: '<{([{{}}[<[[[<>{}]]]>[]]').finish_chars
+  end
+
+  it 'finds the middle score' do
+    input = File.read('fixtures/input.txt')
+    calc = Calculator.from_raw(input)
+
+    assert_equal 288_957, Calculator.find_middle_score(scores: calc.lines.map(&:finish_score))
   end
 end
